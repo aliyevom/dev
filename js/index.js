88ee695c0eb2ba98bd7626d46175c7b49e452071
aliyevom <o.aliyev001@umb.edu>
2023-10-27 01:12:44 -0400

    release ID1:12

--- js/index.js
+++ js/index.js
@@ -93,59 +93,53 @@ function displaySection(link,section) {
 
 // dark and light mode 
 document.documentElement.style.setProperty('--background-color', '#fafafa');
+
 function switchDarkLightMode() {
     let icon = document.querySelector("#switch-container img");
-   
-    icon.onclick = () => {
-    let darkColor = "#111216"; 
-    let darkTextColor = "#999";
-    let darkTextTitleColor = "#eee";
-    let darkTextHoverColor = "#ddd";
-    let darkAccentColor = "#222";
-    icon.onmouseover = () => {
-        console.log("James");
-        icon.setAttribute("src","images/moon-full.svg");
-    }
-    icon.onmouseout = () => {
-        icon.setAttribute("src","images/moon.svg");
-    }
-    let color = getComputedStyle(document.documentElement).getPropertyValue("--background-color");
-    if(color == "#fafafa"){
-        icon.setAttribute("src","images/sun.svg");
-        document.documentElement.style.setProperty('--background-color',darkColor);
-        document.documentElement.style.setProperty('--text-color',darkTextColor);
-        document.documentElement.style.setProperty('--text-title-color',darkTextTitleColor);
-        document.documentElement.style.setProperty('--text-hover-color',darkTextHoverColor);
-        document.documentElement.style.setProperty('--light-accent-color',darkAccentColor);
-        icon.onmouseover = () => {
-                icon.setAttribute("src","images/sun-filled.svg");
-        }
-        icon.onmouseout = () => {
-                icon.setAttribute("src","images/sun.svg");
-        }
-    
-    } else {
-        icon.setAttribute("src","images/moon.svg");
+    let isDarkMode = false;
+
+    function setLightMode() {
         document.documentElement.style.setProperty('--background-color', '#fafafa');
         document.documentElement.style.setProperty('--text-color', '#111');
-        document.documentElement.style.setProperty('--text-hover-color',"#000");
-        document.documentElement.style.setProperty('--text-title-color',"#111");
-        document.documentElement.style.setProperty('--light-accent-color',"#ccc");
+        document.documentElement.style.setProperty('--text-hover-color', '#000');
+        document.documentElement.style.setProperty('--text-title-color', '#111');
+        document.documentElement.style.setProperty('--light-accent-color', '#ccc');
+        icon.setAttribute("src", "moon.svg");
+        isDarkMode = false;
+    }
 
-        icon.onmouseover = () => {
-            console.log("James");
-            icon.setAttribute("src","images/moon-full.svg");
-        }
-        icon.onmouseout = () => {
-            icon.setAttribute("src","images/moon.svg");
+    function setDarkMode() {
+        document.documentElement.style.setProperty('--background-color', '#111216');
+        document.documentElement.style.setProperty('--text-color', '#999');
+        document.documentElement.style.setProperty('--text-title-color', '#eee');
+        document.documentElement.style.setProperty('--text-hover-color', '#ddd');
+        document.documentElement.style.setProperty('--light-accent-color', '#222');
+        icon.setAttribute("src", "sun.svg");
+        isDarkMode = true;
+    }
+
+    function toggleMode() {
+        if (isDarkMode) {
+            setLightMode();
+        } else {
+            setDarkMode();
         }
     }
-    }
+
+    icon.addEventListener("click", toggleMode);
+
+    icon.addEventListener("mouseover", function() {
+        icon.setAttribute("src", isDarkMode ? "sun-filled.svg" : "moon-full.svg");
+    });
+
+    icon.addEventListener("mouseout", function() {
+        icon.setAttribute("src", isDarkMode ? "sun.svg" : "moon.svg");
+    });
 }
-  
 
-
+// Call the function to enable the dark/light mode switch
 switchDarkLightMode();
+
 displaySection("about-me","about-me-section");
 displaySection("skills","skills-section");
 displaySection("projects","projects-section");
